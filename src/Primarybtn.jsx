c55ca73e0bbe78e21e5a389d4a9b5790b3f2fa3b
Mohamed Yansane <yansane472@gmail.com>
2023-03-21 09:18:22 +0000

    adding new methode inside

--- src/Primarybtn.jsx
+++ src/Primarybtn.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const Primarybtn = () => {
+export const Primarybtn = ({btn_name}) => {
   return (
-    <button type="submit" className="p-btn">Go</button>
+    <button type="submit" className="p-btn">{btn_name}</button>
 
   )
 }
